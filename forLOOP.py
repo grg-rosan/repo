6aee2338d8c783a9f284e1803c0cd4064e5eeceb
Roshan grg <grg.rosan99@gmail.com>
2024-08-01 10:14:42 +0545

    hello

---
+++ forLOOP.py
@@ -0,0 +1,20 @@
+
+import random
+# for x in range(5):
+#     print(x)
+    
+# Fruits=["apple","cherrry","mango","banana","kera"]
+# for y in Fruits:
+#     print(y)
+#     if y == "mango":
+#         break
+
+
+# Fruits=["apple","cherrry","mango","banana","kera"]
+# for y in "banana":
+#     print(y)
+# ********************************While loop********************************
+x=1
+while x<10:
+    print(x)
+    x=x+1
